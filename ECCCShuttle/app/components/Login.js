3be7216d06d9a6c5ea6975e110b1dbcf47d3c1b3
Pegasids <tongtongcanopus@gmail.com>
2017-05-11 16:49:58 -0600

    Combined components

--- ECCCShuttle/app/components/Login.js
+++ ECCCShuttle/app/components/Login.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  StatusBar,
+  Image,
   Text,
   TextInput,
   TouchableOpacity,
@@ -20,35 +22,78 @@ export default class Login extends Component {
 
   render() {
     return (
-      <View>
-          <TextInput style={styles.input}
-            placeholder='Email address'
-            keyboardType='email-address'
-            multiline={false}
-            autoCapitalize='none'
-            autoCorrect={false}
-            underlineColorAndroid={'transparent'}
-            returnKeyType='go'
-            onSubmitEditing={this.loginOnPress}
-          />
-          <View style={styles.buttonContainer}>
-                <TouchableOpacity style={styles.c1} onPress={this.signUpOnPress}>
-                    <View>
-                        <Text style={styles.button}>Sign up</Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.c2} onPress={this.loginOnPress}>
-                    <View>
-                        <Text style={styles.button}>Login</Text>
-                    </View>
-                </TouchableOpacity>
-          </View>
-      </View>
+        <View style={styles.container}>
+            <StatusBar
+            backgroundColor='black'
+            />
+            <View>
+                <Image source={require('./../../assets/logo.png')} style={styles.image} />
+                <View style={styles.line} />
+                <Text style={styles.title}>
+                <Text style={styles.titleFirstHalf}>
+                    Park & Ride
+                </Text>
+                <Text style={styles.titleSecondHalf}>
+                    &nbsp;Reward Program
+                </Text>
+                </Text>
+                <TextInput style={styles.input}
+                    placeholder='Email address'
+                    keyboardType='email-address'
+                    multiline={false}
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    underlineColorAndroid={'transparent'}
+                    returnKeyType='go'
+                    onSubmitEditing={this.loginOnPress}
+                />
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.c1} onPress={this.signUpOnPress}>
+                        <View>
+                            <Text style={styles.button}>Sign up</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.c2} onPress={this.loginOnPress}>
+                        <View>
+                            <Text style={styles.button}>Login</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+    },
+    image: {
+        top: -101,
+        resizeMode: 'contain',
+        width: 280,
+    },
+    line: {
+        top: -96,
+        width: 280,
+        height: 1.5,
+        backgroundColor: 'white'
+    },
+    title: {
+        top: -92.5,
+        color: 'white',
+        fontSize: 21,
+    },
+    titleFirstHalf: {
+        fontFamily: 'Raleway-LightItalic',
+    },
+    titleSecondHalf: {
+        fontFamily: 'Raleway-Light',
+    },
     input: {
         top: -25,
         width:280,
@@ -91,3 +136,5 @@ const styles = StyleSheet.create({
         textAlignVertical: 'center'
     }
 });
+
+AppRegistry.registerComponent('Login', () => Login);
--- ECCCShuttle/index.android.js
+++ ECCCShuttle/index.android.js
@@ -12,30 +12,17 @@ import {
   View
 } from 'react-native';
 
-import Logo from './app/components/Logo';
 import Login from './app/components/Login';
 
 export default class ECCCShuttle extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar
-          backgroundColor='black'
-        />
-        <Logo/>
         <Login/>
-      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-  },
 });
 
 AppRegistry.registerComponent('ECCCShuttle', () => ECCCShuttle);
