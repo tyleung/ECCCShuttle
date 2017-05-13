f77d83bf68bd2f9acf9c63dbe40083a1315e507f
tyleung <tyleung@ualberta.ca>
2017-05-12 20:45:55 -0600

    added transaction_type table

---
+++ shuttleapi/database/migrations/2017_05_13_013318_create_transaction_type_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_type', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type', 255)->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transaction_type');
+    }
+}
