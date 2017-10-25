2088ebd772a7b551cbc1017f52f93464aabec3e2
tyleung <tyleung@ualberta.ca>
2017-10-24 23:47:33 -0600

    qr scanner styling

--- mobile/app/components/BarcodeScanner.js
+++ mobile/app/components/BarcodeScanner.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 import { BarCodeScanner, Permissions } from "expo";
 import CryptoJSAesJson, { CRYPTO_KEY } from "../utils/CryptoJSAesJson";
 
@@ -23,7 +29,7 @@ export default class BarcodeScannerExample extends React.Component {
         })
         .toString(CryptoJS.enc.Utf8)
     );
-    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    
     alert(`type: ${type}, decrypted: ${decrypted}`);
   };
 
@@ -35,15 +41,74 @@ export default class BarcodeScannerExample extends React.Component {
     } else if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
     } else {
+      const { navigate } = this.props.navigation;
       return (
         <View style={{ flex: 1 }}>
           <BarCodeScanner
             onBarCodeRead={this._handleBarCodeRead}
             barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
             style={StyleSheet.absoluteFill}
-          />
+          >
+            <View style={styles.customRectangleContainer}>
+              <View style={styles.customOutsideMarker}>
+                <Text style={styles.customTitleText}>Scan QR Code</Text>
+              </View>
+              <View style={{ height: 250, flexDirection: "row" }}>
+                <View style={styles.customOutsideMarker} />
+                <View style={styles.customRectangle} />
+                <View style={styles.customOutsideMarker} />
+              </View>
+              <View
+                style={[
+                  styles.customOutsideMarker,
+                  { justifyContent: "flex-end", alignItems: "center" }
+                ]}
+              >
+                <TouchableOpacity
+                  style={{ marginBottom: 50 }}
+                  onPress={() => navigate("MainScreen")}
+                >
+                  <Text style={styles.customBottomText}>Cancel</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </BarCodeScanner>
         </View>
       );
     }
   }
 }
+
+const styles = StyleSheet.create({
+  zeroContainer: {
+    height: 0,
+    flex: 0
+  },
+  customRectangleContainer: {
+    flex: 0,
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width,
+    flexDirection: "column"
+  },
+  customRectangle: {
+    height: 250,
+    width: 250,
+    borderWidth: 1,
+    borderColor: "white",
+    backgroundColor: "transparent"
+  },
+  customOutsideMarker: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)"
+  },
+  customTitleText: {
+    color: "white",
+    fontSize: 22,
+    textAlign: "center",
+    marginTop: 20
+  },
+  customBottomText: {
+    color: "white",
+    fontSize: 22
+  }
+});
